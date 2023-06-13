636f9af3b02e502869c43dc95a5a34c720440c38
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-13 18:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let currentMusic;
+
+let buttons = document.querySelectorAll(".btn");
+
+for(let i=0;i<buttons.length;i++){
+	buttons[i].addEventListener('click',(e)=>{
+		// console.log(buttons[i].innerText)
+		let musicToPlay = document.getElementById(`${buttons[i].innerText}`)
+		if(currentMusic){
+			currentMusic.pause();
+		}
+		if(musicToPlay){
+			musicToPlay.play();
+			currentMusic=musicToPlay;
+		}
+	})
+}
+
+let stop = document.getElementById("stop");
+stop.addEventListener('click',(e)=>{
+	if(currentMusic){
+		currentMusic.pause();
+	}
+})
